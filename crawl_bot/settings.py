c39837c63bb1efe8a9251e2665538796ffb87827
harikrishnan <harikrishnan.a@infibeam.net>
2017-03-31 22:15:27 +0530

    removing http cache

--- crawl_bot/settings.py
+++ crawl_bot/settings.py
@@ -99,7 +99,7 @@ COOKIES_ENABLED = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-HTTPCACHE_ENABLED = True
+# HTTPCACHE_ENABLED = True
 #HTTPCACHE_EXPIRATION_SECS = 0
 HTTPCACHE_DIR = 'httpcache'
 HTTPCACHE_IGNORE_HTTP_CODES = []
